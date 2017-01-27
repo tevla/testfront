c21033fe2fc4acc4f2d66c343b4a3ecbb0ba1e3e
Vladimir@Tevla <vladimir@tevla.co.uk>
2017-01-27 13:14:21 +0000

    first php test on server

---
+++ testdb.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "34.248.40.124";
+$username = "webaccess";
+$password = "qwerty";
+$dbname = "huddle";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+	echo "Connection successful.";
+}
+
+$sql = "SELECT * FROM persons";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["personid"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
